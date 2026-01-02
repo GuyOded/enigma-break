16f13d038008619364523d4ef56f33eba56e0d2f
Guy Oded <guy@example.com>
2026-01-02 11:44:38 +0200

    Update dependency version
    Add stub for enumerating rotor permutations

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,20 @@ use enigma::reflectors;
 use enigma::rotor::rotors;
 
 fn main() {
-    let left = rotors::create_rotor_1();
-    let mid = rotors::create_rotor_2();
-    let right = rotors::create_rotor_3();
-    let reflector = reflectors::create_reflector_a();
+    let rotor_1 = rotors::create_rotor_1();
+    let rotor_2 = rotors::create_rotor_2();
+    let rotor_3 = rotors::create_rotor_3();
+    let rotor_4 = rotors::create_rotor_4();
+    let rotor_5 = rotors::create_rotor_5();
+    let reflector_a = reflectors::create_reflector_a();
+    let reflector_b = reflectors::create_reflector_b();
+    let reflector_c = reflectors::create_reflector_c();
 
     let enigma = Enigma::new(left, mid, right, reflector);
 
     todo!("Break the cipher!")
+}
+
+fn enumerate_n_choose_m<const n: u32, const m: u32>(n: u32, m: u32) -> impl Iterator<Item = &[i32:m]> {
+    todo!()
 }
